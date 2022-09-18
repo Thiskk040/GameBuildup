bb0f70ff8063e15c839bd689c1be2dcfcec49015
Thiskk040 <Khunanon.kamin@gmailc.om>
2022-09-18 10:06:41 +0700

    Finished version beta

---
+++ Assets/Asset/winspot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class winspot : MonoBehaviour
+{
+    public string nextSceneName;
+    public bool lastLevel;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+           if(lastLevel == true)
+            {
+                Debug.Log("You win");
+            }
+           else
+            {
+                SceneManager.LoadScene(nextSceneName);
+            }
+        }
+    }
+}
